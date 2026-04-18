7b44cd26a1053719692d6b12265f13e06f95b3ea
Leander Muskalla <muskalla@sozialit.de>
2026-04-18 11:16:56 +0200

    Improved LoadingIndicator

--- src/lib/components/ui/LoadingIndicator.svelte
+++ src/lib/components/ui/LoadingIndicator.svelte
@@ -1,51 +1,50 @@
-<!--suppress CssUnusedSymbol -->
 <script lang="ts">
-	import { getLoadingIndicator } from '$lib/state/LoadingIndicator.svelte.js';
+	import { getLoadingIndicator } from '$lib/state/LoadingIndicator.svelte';
 
 	const loadingIndicator = getLoadingIndicator();
 
 	let active = $derived(loadingIndicator.active);
 	let progress = $derived(loadingIndicator.progress);
-	let heightClass = 'h-[3px]';
 	let colorClass = 'bg-c-btn';
 </script>
 
-<div class="pointer-events-none fixed top-0 right-0 left-0 z-50">
-	{#if active}
-		<div class={`relative ${heightClass}`}>
+<div class="pointer-events-none fixed top-0 right-0 left-0 z-[200] w-full">
+	<!--{#if active}-->
+		<div class="relative h-[3px]">
 			{#if progress === null}
-				<div class={`absolute inset-0 overflow-hidden ${heightClass}`}>
-					<div class={`absolute inset-0 ${heightClass} ${colorClass} opacity-20`}></div>
-					<div class={`absolute top-0 ${heightClass} ${colorClass} indeterminate`}></div>
+				<div class="absolute inset-0 overflow-hidden h-full">
+					<div class="absolute inset-0 h-full opacity-20 bg-gradient-to-r from-c-neutral to-c-action"></div>
+					<div class="absolute top-0 h-full indeterminate bg-gradient-to-l from-c-action to-c-neutral"></div>
 				</div>
 			{:else}
 				<div
-					class={`${heightClass} ${colorClass} transition-[width] duration-300 ease-out`}
+					class="h-full transition-all duration-300 ease-out {colorClass}"
 					style={`width: ${Math.min(100, Math.max(0, progress * 100)).toFixed(1)}%`}
 				></div>
 			{/if}
 		</div>
-	{/if}
+	<!--{/if}-->
 </div>
 
 <style>
-	.indeterminate {
-		width: 20%;
-		left: -20%;
-		animation: loading-bar 1.2s cubic-bezier(0.4, 0, 0.2, 1) infinite;
-	}
-	@keyframes loading-bar {
-		0% {
-			left: -20%;
-			width: 20%;
-		}
-		50% {
-			left: 50%;
-			width: 30%;
-		}
-		100% {
-			left: 100%;
-			width: 15%;
-		}
-	}
+    .indeterminate {
+        width: 30%;
+        left: -30%;
+        animation: loading-bar 1.4s cubic-bezier(0.4, 0.0, 0.6, 1) infinite;
+    }
+
+    @keyframes loading-bar {
+        0% {
+            left: -30%;
+            width: 30%;
+        }
+        60% {
+            left: 60%;
+            width: 50%;
+        }
+        100% {
+            left: 110%;
+            width: 30%;
+        }
+    }
 </style>
