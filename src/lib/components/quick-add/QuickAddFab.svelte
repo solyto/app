13b984ad9a6632f6a9d6fb09d903b3caf455e419
Leander Muskalla <muskalla@sozialit.de>
2026-05-12 17:38:34 +0200

    QuickAdd button solyto hover

--- src/lib/components/quick-add/QuickAddFab.svelte
+++ src/lib/components/quick-add/QuickAddFab.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <button
-	class="fixed right-6 bottom-24 z-30 flex h-14 w-14 cursor-pointer items-center justify-center rounded-full bg-c-action text-white shadow-lg transition-all hover:shadow-xl hover:scale-105 active:scale-95 2xl:right-10 2xl:bottom-10"
+	class="fixed right-6 bottom-24 z-30 flex h-14 w-14 cursor-pointer items-center justify-center rounded-full bg-c-action text-white shadow-lg transition-all hover:bg-c-warning hover:shadow-xs 2xl:right-10 2xl:bottom-10"
 	onclick={() => quickAdd.openModal()}
 	title={ts.get.quick_add.title}
 	in:scale={{ start: 0.5 }}
