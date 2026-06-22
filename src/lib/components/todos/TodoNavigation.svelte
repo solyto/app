7bda139d1bf68897f5a567bfa195f580e2149ba2
Leander Muskalla <muskalla@sozialit.de>
2026-06-22 10:33:16 +0200

    Todo Navigation order

--- src/lib/components/todos/TodoNavigation.svelte
+++ src/lib/components/todos/TodoNavigation.svelte
@@ -8,11 +8,28 @@
 
 	const sections: TodoNavigationSection[] = [
 		{
+			header: ts.get.todos.due_at,
+			items: [
+				{ filter: { type: 'due', value: 'today' }, label: ts.get.todos.due_today },
+				{ filter: { type: 'due', value: 'tomorrow' }, label: ts.get.todos.due_tomorrow },
+				{ filter: { type: 'due', value: 'week' }, label: ts.get.todos.due_week },
+				{ filter: { type: 'due', value: 'overdue' }, label: ts.get.todos.due_overdue }
+			]
+		},
+		{
 			header: ts.get.todos.priority,
 			items: [
 				{ filter: { type: 'priority', value: 'high' }, label: ts.get.todos.priority_high },
 				{ filter: { type: 'priority', value: 'medium' }, label: ts.get.todos.priority_medium },
 				{ filter: { type: 'priority', value: 'low' }, label: ts.get.todos.priority_low }
+			]
+		},
+		{
+			header: ts.get.todos.effort,
+			items: [
+				{ filter: { type: 'effort', value: 'low' }, label: ts.get.todos.effort_low },
+				{ filter: { type: 'effort', value: 'medium' }, label: ts.get.todos.effort_medium },
+				{ filter: { type: 'effort', value: 'high' }, label: ts.get.todos.effort_high }
 			]
 		},
 		{
@@ -25,23 +42,6 @@
 				{ filter: { type: 'status', value: 'almost-done' }, label: ts.get.todos.status_almost_done }
 			]
 		},
-		{
-			header: ts.get.todos.effort,
-			items: [
-				{ filter: { type: 'effort', value: 'low' }, label: ts.get.todos.effort_low },
-				{ filter: { type: 'effort', value: 'medium' }, label: ts.get.todos.effort_medium },
-				{ filter: { type: 'effort', value: 'high' }, label: ts.get.todos.effort_high }
-			]
-		},
-		{
-			header: ts.get.todos.due_at,
-			items: [
-				{ filter: { type: 'due', value: 'today' }, label: ts.get.todos.due_today },
-				{ filter: { type: 'due', value: 'tomorrow' }, label: ts.get.todos.due_tomorrow },
-				{ filter: { type: 'due', value: 'week' }, label: ts.get.todos.due_week },
-				{ filter: { type: 'due', value: 'overdue' }, label: ts.get.todos.due_overdue }
-			]
-		}
 	];
 </script>
 
